7c00403fd90ac9a260eb593d562aaf1b38bed83b
Sergio Cosman <cosmansy@mcmaster.ca>
2015-04-05 18:36:54 -0400

    added files

---
+++ src/Competitor.java
@@ -0,0 +1,25 @@
+//Competitor.java
+
+public class Competitor {
+	private final String name;
+	private final int rank;
+	private final String region;
+
+	public Competitor(String name, int rank, String region) {
+		this.name = name;
+		this.rank = rank;
+		this.region = region;
+	}
+
+	public int getRank() {
+		return this.rank;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getRegion() {
+		return this.region;
+	}
+}
