183c64efb52f99528f34a1f92be6716f60259ebc
Zed Ahmad <zed.aziz.ahmad@gmail.com>
2015-04-10 18:25:46 -0400

    [ZA] Created PoolGenModel

--- src/PoolGenModel.java
+++ src/PoolGenModel.java
@@ -1,8 +1,55 @@
+import java.io.File;
+import java.util.Map;
+
 /**
  * Author: Zed
  * Date: 4/3/2015
  */
 public class PoolGenModel {
-    // Main class
-    // TODO
+    // Map of seeds to competitors
+    private Map<Integer, Competitor> competitors;
+    private File competitorsFile;
+    private String competitorsFilePath;
+    private int bracketSize;
+    private int numExitCompetitors;
+
+    public Map<Integer, Competitor> getCompetitors() {
+        return competitors;
+    }
+
+    public void setCompetitors(Map<Integer, Competitor> competitors) {
+        this.competitors = competitors;
+    }
+
+    public File getCompetitorsFile() {
+        return competitorsFile;
+    }
+
+    public void setCompetitorsFile(File competitorsFile) {
+        this.competitorsFile = competitorsFile;
+    }
+
+    public String getCompetitorsFilePath() {
+        return competitorsFilePath;
+    }
+
+    public void setCompetitorsFilePath(String competitorsFilePath) {
+        this.competitorsFilePath = competitorsFilePath;
+    }
+
+    public int getBracketSize() {
+        return bracketSize;
+    }
+
+    public void setBracketSize(int bracketSize) {
+        this.bracketSize = bracketSize;
+    }
+
+    public int getNumExitCompetitors() {
+        return numExitCompetitors;
+    }
+
+    public void setNumExitCompetitors(int numExitCompetitors) {
+        this.numExitCompetitors = numExitCompetitors;
+    }
 }
